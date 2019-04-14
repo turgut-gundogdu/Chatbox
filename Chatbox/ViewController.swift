575b757701fd77d64135b4671cf50ddaabc65880
Akif Turgut Gündoğdu <akifturgutgundogdu@Solitude.local>
2019-04-14 21:40:50 +0200

    First Commit

---
+++ Chatbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatbox
+//
+//  Created by Akif Turgut Gündoğdu on 14.04.2019.
+//  Copyright © 2019 Akif Turgut Gündoğdu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
